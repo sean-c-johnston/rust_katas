b083c1b529393d61bd0ee7e3739bcc61955d0930
Sean Johnston <sean.c.johnston@gmail.com>
2024-07-13 00:29:04 +0100

    add first test for addition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+// $5 + $5 = $10
+// $5 + £5 = $15 if 2:1 rate
+#[cfg(test)]
+#[test]
+fn test_addition() {
+    let five_dollars = Dollar { amount: 5 };
+    let ten_dollars = five_dollars.add(5);
+
+    assert_eq!(10, ten_dollars.amount);
+}
